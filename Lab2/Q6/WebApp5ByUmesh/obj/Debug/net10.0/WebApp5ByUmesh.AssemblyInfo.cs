c532f4d6900cd8c3d52694bfc95b27308697bfef
Strange <umeshpariyar112@gmail.com>
2026-09-09 13:56:54 +0545

    Add Q6 Docker configuration

--- Lab2/Q6/WebApp5ByUmesh/obj/Debug/net10.0/WebApp5ByUmesh.AssemblyInfo.cs
+++ Lab2/Q6/WebApp5ByUmesh/obj/Debug/net10.0/WebApp5ByUmesh.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp5ByUmesh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0331c3497d86d11f405cc2d68aefc88ab75a8e3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59de1edae1fbbcf3b8eca283f3d1465a76190a41")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp5ByUmesh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp5ByUmesh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
