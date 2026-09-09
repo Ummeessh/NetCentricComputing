1539ad911f6bf8513c35a099daa6f0c2480fc206
Strange <umeshpariyar112@gmail.com>
2026-09-09 14:17:42 +0545

    Added Sqlite

--- Lab2/Q6/WebApp5ByUmesh/Controllers/StudentController.cs
+++ Lab2/Q6/WebApp5ByUmesh/Controllers/StudentController.cs
@@ -1,27 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 using WebApp5ByUmesh.Models;
 
-namespace WebApp5ByUmesh.Controllers
+namespace WebApp5ByJayson.Controllers
 {
     public class StudentController : Controller
     {
-        private readonly string connectionString = "Server=localhost,1433;Database=CollegeDB;User Id=sa;Password=Umesh@12345#;TrustServerCertificate=True;";
+        private readonly string connectionString = "Data Source=CollegeDB.db;";
 
         public StudentController()
         {
-            using (var con = new SqlConnection(connectionString))
+            using (var con = new SqliteConnection(connectionString))
             {
                 con.Open();
-                string query = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Students')
-                                 BEGIN
-                                     CREATE TABLE Students (
-                                         Id INT PRIMARY KEY IDENTITY(1,1),
-                                         Name NVARCHAR(50) NOT NULL,
-                                         Faculty NVARCHAR(50) NOT NULL
-                                     );
-                                 END";
-                using (var cmd = new SqlCommand(query, con))
+                string query = @"CREATE TABLE IF NOT EXISTS Students (
+                                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    Name TEXT NOT NULL,
+                                    Faculty TEXT NOT NULL
+                                );";
+                using (var cmd = new SqliteCommand(query, con))
                 {
                     cmd.ExecuteNonQuery();
                 }
@@ -32,11 +29,11 @@ namespace WebApp5ByUmesh.Controllers
         public IActionResult Index()
         {
             List<Student> students = new List<Student>();
-            using (var con = new SqlConnection(connectionString))
+            using (var con = new SqliteConnection(connectionString))
             {
                 con.Open();
                 string query = "SELECT Id, Name, Faculty FROM Students";
-                using (var cmd = new SqlCommand(query, con))
+                using (var cmd = new SqliteCommand(query, con))
                 using (var reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
@@ -59,11 +56,11 @@ namespace WebApp5ByUmesh.Controllers
         [HttpPost]
         public IActionResult Create(Student student)
         {
-            using (var con = new SqlConnection(connectionString))
+            using (var con = new SqliteConnection(connectionString))
             {
                 con.Open();
                 string query = "INSERT INTO Students (Name, Faculty) VALUES (@Name, @Faculty)";
-                using (var cmd = new SqlCommand(query, con))
+                using (var cmd = new SqliteCommand(query, con))
                 {
                     cmd.Parameters.AddWithValue("@Name", student.Name);
                     cmd.Parameters.AddWithValue("@Faculty", student.Faculty);
@@ -77,11 +74,11 @@ namespace WebApp5ByUmesh.Controllers
         public IActionResult Edit(int id)
         {
             Student? student = null;
-            using (var con = new SqlConnection(connectionString))
+            using (var con = new SqliteConnection(connectionString))
             {
                 con.Open();
                 string query = "SELECT Id, Name, Faculty FROM Students WHERE Id = @Id";
-                using (var cmd = new SqlCommand(query, con))
+                using (var cmd = new SqliteCommand(query, con))
                 {
                     cmd.Parameters.AddWithValue("@Id", id);
                     using (var reader = cmd.ExecuteReader())
@@ -104,11 +101,11 @@ namespace WebApp5ByUmesh.Controllers
         [HttpPost]
         public IActionResult Edit(Student student)
         {
-            using (var con = new SqlConnection(connectionString))
+            using (var con = new SqliteConnection(connectionString))
             {
                 con.Open();
                 string query = "UPDATE Students SET Name = @Name, Faculty = @Faculty WHERE Id = @Id";
-                using (var cmd = new SqlCommand(query, con))
+                using (var cmd = new SqliteCommand(query, con))
                 {
                     cmd.Parameters.AddWithValue("@Id", student.Id);
                     cmd.Parameters.AddWithValue("@Name", student.Name);
@@ -122,11 +119,11 @@ namespace WebApp5ByUmesh.Controllers
         // DELETE
         public IActionResult Delete(int id)
         {
-            using (var con = new SqlConnection(connectionString))
+            using (var con = new SqliteConnection(connectionString))
             {
                 con.Open();
                 string query = "DELETE FROM Students WHERE Id = @Id";
-                using (var cmd = new SqlCommand(query, con))
+                using (var cmd = new SqliteCommand(query, con))
                 {
                     cmd.Parameters.AddWithValue("@Id", id);
                     cmd.ExecuteNonQuery();
